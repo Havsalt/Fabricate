b20eba456d8b03106d2adc14c7a3044c05d0a12e
Havsalt <knutolaihelgesen@gmail.com>
2024-06-18 14:43:22 +0200

    Switched to regex

--- fabricate/substition.py
+++ fabricate/substition.py
@@ -1,14 +1,45 @@
-from typing import Generator as _Generator
+import re as _re
+from functools import partial as _partial
+
+from actus import error as _error
 
 
-def collect_format_keys(format_string: str) -> _Generator[str, None, None]:
-    remaining = format_string
-    dummy_kwargs: dict[str, None] = {}
-    while remaining:
-        try:
-            _formatted = format_string.format(**dummy_kwargs)
-        except KeyError as lookup_error:
-            for key in lookup_error.args:
-                dummy_kwargs.setdefault(key, None)
-                yield key
-        remaining = remaining[1:]
+_KEY_PATTERN = _re.compile(
+    r"\$inject\[(\w+)\]",
+    flags=_re.RegexFlag.ASCII
+)
+
+
+def collect_format_keys(
+    format_string: str
+) -> list[str]:
+    return _KEY_PATTERN.findall(format_string)
+
+
+def _replace_keyword(
+    match: _re.Match[str],
+    /,
+    *,
+    pairs: dict[str, str],
+    blueprint_name: str
+) -> str:
+    key = match.group(1)
+    if key in pairs:
+        return pairs[key]
+    _error(f"No value found for $[{key}] in blueprint $[{blueprint_name}]")
+    return key
+
+
+def inject_values(
+    string: str,
+    /,
+    *,
+    pairs: dict[str, str],
+    blueprint_name: str
+) -> str:
+    partial_replace_keyword = _partial(
+        _replace_keyword,
+        pairs=pairs,
+        blueprint_name=blueprint_name
+    )
+    return _KEY_PATTERN.sub(partial_replace_keyword, string)
